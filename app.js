ae0a5a4fc22dc7efd9b311b1c1b194743feb6b94
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-01 22:51:37 -0300

    M04C06 - Trabajando con formularios

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.get(["/", "/home"], (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
